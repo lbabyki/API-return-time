6d33d922cbc66221d9f5836a845931ed813931a5
caing <crglh25@gmail.com>
2025-05-25 19:08:25 +0700

    them

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 3026;
+const cors = require("cors");
+app.use(cors());
 
 app.get("/time", (req, res) => {
   const now = new Date();
